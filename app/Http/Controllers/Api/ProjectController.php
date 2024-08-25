7945e84c93a5baba6a27f7b3bc92633ed213c7d8
RiccardoTolusso <riccardotolusso215@gmail.com>
2024-08-25 22:19:24 +0200

    END OF EXERCISE

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -10,10 +10,22 @@ class ProjectController extends Controller
 {
     public function index()
     {
-        $projects = Project::paginate(10);
+        $projects = Project::with('type')->paginate(3);
         return response()->json([
             'status' => true,
             'result' => $projects,
+        ]);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show($id)
+    {
+        $project = Project::with('type', 'technologies')->where('id', $id)->first();
+        return response()->json([
+            "success" => true,
+            "results" => $project,
         ]);
     }
 }
